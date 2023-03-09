1513e08c1e3ee35cfb8ca0bda767aad03ce031c0
CodingChick <awesomecoding26@gmail.com>
2023-03-08 21:59:28 -0800

    feat: adding mobile functionality

--- src/components/article-card/ArticleCard.tsx
+++ src/components/article-card/ArticleCard.tsx
@@ -1,13 +1,20 @@
-import { MobileFooter } from "../mobile-footer/MobileFooter";
-import { MobileSocialFooter } from "../mobile-social-footer/MobileSocialFooter";
+import { useState } from "react";
 import { DesktopFooter } from "../desktop-footer/DesktopFooter";
 import { DesktopSocialFooter } from "../desktop-social-footer/DesktopSocialFooter";
 import DeskImg from "../../../images/drawers.jpg";
+import { ActiveSocialShareIcon } from "../icons/ActiveSocialShareIcon";
+import { SocialShareIcon } from "../icons/SocialShareIcon";
+import FacebookIcon from "../../../images/icon-facebook.svg";
+import TwitterIcon from "../../../images/icon-twitter.svg";
+import PinterestIcon from "../../../images/icon-pinterest.svg";
+import MichelleAvatar from "../../../images/avatar-michelle.jpg";
 import "../article-card/ArticleCard.css";
-import { useState } from "react";
 
 export const ArticleCard = () => {
-  const [isSocialMediaBarShowing, setIsSocialMediaBarShowing] = useState(true);
+  const [isSocialMediaBarShowing, setIsSocialMediaBarShowing] = useState(false);
+
+  const toggleSocialMediaBar = () =>
+    setIsSocialMediaBarShowing(!isSocialMediaBarShowing);
 
   return (
     <div className="card">
@@ -25,9 +32,43 @@ export const ArticleCard = () => {
         </p>
       </div>
       {isSocialMediaBarShowing ? (
-        <MobileSocialFooter isSocialMediaBarShowing={isSocialMediaBarShowing} />
+        <footer className="social-icon-footer flex-container">
+          <div className="icon-and-text-container flex-container">
+            <p className="share-text">share</p>
+            <div className="icon-container flex-container">
+              <img className="icon" src={FacebookIcon} alt="" />
+              <img className="icon" src={TwitterIcon} alt="" />
+              <img className="icon" src={PinterestIcon} alt="" />
+            </div>
+          </div>
+          <button
+            onClick={toggleSocialMediaBar}
+            className="social-share-btn active-social-bar-btn"
+          >
+            {isSocialMediaBarShowing ? (
+              <ActiveSocialShareIcon />
+            ) : (
+              <SocialShareIcon />
+            )}
+          </button>
+        </footer>
       ) : (
-        <MobileFooter />
+        <footer className="mobile-footer">
+          <div className="avatar-container">
+            <img
+              className="avatar"
+              src={MichelleAvatar}
+              alt="michelle avatar"
+            />
+            <div className="avatar-text">
+              <p className="avatar-name">Michelle Appleton</p>
+              <p className="avatar-date">28 Jun 2020</p>
+            </div>
+          </div>
+          <button onClick={toggleSocialMediaBar} className="social-share-btn">
+            <SocialShareIcon />
+          </button>
+        </footer>
       )}
     </div>
   );
