28c7973f2b849dd407f113252db31f82db54fca3
CodingChick <awesomecoding26@gmail.com>
2023-03-08 23:11:15 -0800

    feat: adding desktop styles

--- src/components/article-card/ArticleCard.tsx
+++ src/components/article-card/ArticleCard.tsx
@@ -3,10 +3,13 @@ import { DesktopFooter } from "../desktop-footer/DesktopFooter";
 import { DesktopSocialFooter } from "../desktop-social-footer/DesktopSocialFooter";
 import DeskImg from "../../../images/drawers.jpg";
 import { MobileFooter } from "../mobile-footer/MobileFooter";
+import { Footer } from "../footer/Footer";
 import "../article-card/ArticleCard.css";
 
 export const ArticleCard = () => {
   const [isSocialMediaBarShowing, setIsSocialMediaBarShowing] = useState(false);
+  //TODO: set to innerwidth
+  const [isDesktopViewShowing, setIsDesktopViewShowing] = useState(true);
 
   const toggleSocialMediaBar = () =>
     setIsSocialMediaBarShowing(!isSocialMediaBarShowing);
@@ -14,22 +17,24 @@ export const ArticleCard = () => {
   return (
     <div className="card">
       <img className="hero-img" src={DeskImg} alt="desk" />
-
-      <div className="text-container">
-        <p className="tag-line">
-          Shift the overall look and feel by adding these wonderful touches to
-          furniture in your home
-        </p>
-        <p className="description">
-          Ever been in a room and felt like something was missing? Perhaps it
-          felt slightly bare and uninviting. I&apos;ve got some simple tips to
-          help you make any room feel complete.
-        </p>
+      <div className="text-footer-container">
+        <div className="text-container">
+          <p className="tag-line">
+            Shift the overall look and feel by adding these wonderful touches to
+            furniture in your home
+          </p>
+          <p className="description">
+            Ever been in a room and felt like something was missing? Perhaps it
+            felt slightly bare and uninviting. I&apos;ve got some simple tips to
+            help you make any room feel complete.
+          </p>
+        </div>
+        <Footer toggleSocialMediaBar={toggleSocialMediaBar} />
+        {/* <MobileFooter
+          isSocialMediaBarShowing={isSocialMediaBarShowing}
+          toggleSocialMediaBar={toggleSocialMediaBar}
+        /> */}
       </div>
-      <MobileFooter
-        isSocialMediaBarShowing={isSocialMediaBarShowing}
-        toggleSocialMediaBar={toggleSocialMediaBar}
-      />
     </div>
   );
 };
